4aa07474982f51b11797ce967ab1273ac019c5c6
Vasant Marur <vasantm.code@gmail.com>
2017-09-18 12:02:22 -0400

    changed the ways date input is defined, using shiny function dateRangeInput

--- GetPubMed/ui.R
+++ GetPubMed/ui.R
@@ -11,6 +11,7 @@ library(shiny)
 library(shinythemes)
 library(shinyjs)
 library(shinycssloaders)
+#source("global.R")
 shinyUI(fluidPage(theme=shinytheme("united"),
                   useShinyjs(),
                   headerPanel("PubMed Search"),
@@ -19,11 +20,16 @@ shinyUI(fluidPage(theme=shinytheme("united"),
                       helpText("Type a word below and search PubMed to find documents that contain that word in the text. You can even type multiple words. You can search authors, topics, any acronym, etc."),
                       textInput("query", label = h3("Keyword(s)"),
                                 value = "APOE4 AND (polyunsaturated OR monounsaturated OR saturated)"),
-                      helpText("You can specify the start and end dates of your search,
-                               use the format YYYY/MM/DD."),
-                      textInput("date1", label = h3("From"),value="1990/01/01"),
-                      textInput("date2", label = h3("To"),  value = format(Sys.time(),"%Y/%m/%d")),
-                      helpText("To Date is auto populated with today's date"),
+                      helpText("Please specify the start and end dates of your search,
+                               using the format YYYY-MM-DD."),
+                      dateRangeInput("dates","Date Range:",
+                                     start = GetPastDate(Sys.Date()),
+                                     end = Sys.Date(),
+                                     format = "yyyy-mm-dd"),
+                      #textInput("date1", label = h3("From"),value="1990/01/01"),
+                      #textInput("date2", label = h3("To"),  value = format(Sys.time(),"%Y/%m/%d")),
+                  helpText("Past date is set to 30 years before & to Date is auto populated with today's date"),
+
                       actionButton("goButton","Get Abstracts"),
                       helpText("Now click Get Abstracts to get the results."),
                       downloadButton('downloadResults','Download as Text File'),
