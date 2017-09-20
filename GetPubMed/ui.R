bda025f2ca10d9f11987f9e1c9f110919477e9cf
Vasant Marur <vasantm.code@gmail.com>
2017-09-20 18:57:48 -0400

    Added CSS code to style error message dataRangeInput instead of textinput to make it easy for user to choose dates of search Sliderinput for choosing number of abstracts to get

--- GetPubMed/ui.R
+++ GetPubMed/ui.R
@@ -13,27 +13,51 @@ library(shinyjs)
 library(shinycssloaders)
 #source("global.R")
 shinyUI(fluidPage(theme=shinytheme("united"),
+                  tags$head(
+                    tags$style(HTML("
+      .shiny-output-error-myvalidation {
+              color: grey;
+              font-size: x-large;
+              left: 0;
+              line-height: 200px;
+              margin-top: 200px;
+              position: absolute;
+              text-align: center;
+              top: 50%;
+              width: 100%;
+      }
+    "))
+                  ),
                   useShinyjs(),
                   headerPanel("PubMed Search"),
                   sidebarLayout(
                     sidebarPanel(
                       helpText("Type a word below and search PubMed to find documents that contain that word in the text. You can even type multiple words. You can search authors, topics, any acronym, etc."),
-                      textInput("query", label = h3("Keyword(s)"),
-                                value = "APOE4 AND (polyunsaturated OR monounsaturated OR saturated)"),
+                      textInput("query", label = h2("Keyword(s)"),
+                                value = "",placeholder = "Type search term here"),
                       helpText("Please specify the start and end dates of your search,
                                using the format YYYY-MM-DD."),
-                      dateRangeInput("dates","Date Range:",
-                                     start = GetPastDate(Sys.Date()),
-                                     end = Sys.Date(),
-                                     format = "yyyy-mm-dd"),
+                      dateRangeInput("dates", label = h2("Date Range:"),
+                                     start = GetPastDate(years = 30),
+                                     end = format(Sys.Date(),"%Y/%m/%d"),
+                                     format = "yyyy/mm/dd", width = '300px'),
                       #textInput("date1", label = h3("From"),value="1990/01/01"),
                       #textInput("date2", label = h3("To"),  value = format(Sys.time(),"%Y/%m/%d")),
-                  helpText("Past date is set to 30 years before & to Date is auto populated with today's date"),
+                helpText("Past date is set to 30 years before & To Date
+                         is auto populated with today's date"),
+                sliderInput("req_count",
+                             label = h2("Number of Abstracts to get:"),
+                           min = 200, max = 1000, value =200, step = 50,
+                           width = '300px'),
+                helpText("Choose number of Abstracts to retrieve,
+                         Note: higher number takes longer"),
 
                       actionButton("goButton","Get Abstracts"),
                       helpText("Now click Get Abstracts to get the results."),
                       downloadButton('downloadResults','Download as Text File'),
 helpText("Download button is only active after results are returned from PubMed")),
+#actionButton("reset","Clear"),
+#helpText("Hit Clear to clear current search")),
 
                     mainPanel(
                       tabsetPanel(
